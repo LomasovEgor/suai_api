12a34ca53844e6329e0e6869d0b1e0315f1d0546
LomasovEgor <104323430+LomasovEgor@users.noreply.github.com>
2022-09-06 10:52:50 +0300

    update config.py and requirements.txt

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import motor.motor_asyncio
+import os
 
-MONGODB_URL = 'mongodb://localhost:27017/'
+MONGODB_URL = os.environ['MONGODB_URL']
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 
